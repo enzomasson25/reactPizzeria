3b4978204222d8328df833c6cf0f1d576191f80b
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-10-02 15:45:33 +0200

    Create App.js

---
+++ pizzeria/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Salut bogoss.
+        </p>
+    </div>
+  );
+}
+
+export default App;
