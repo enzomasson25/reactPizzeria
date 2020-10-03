fbc2eb76ca9bc0563135a01d9af756d71821c100
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-10-03 12:42:07 +0200

    Update carte.jsx

--- pizzeria/src/components/carte.jsx
+++ pizzeria/src/components/carte.jsx
@@ -1,9 +1,79 @@
 import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
+
+/*
 const Carte = ()=>(
-    <div>
-        Carte
-    </div>
+
+        const classes = useStyles();
+        <Card className={classes.root}>
+        <CardActionArea>
+        <CardMedia
+            className={classes.media}
+            image="/assets/img/4fromages.jpg"
+            title="4 fromages"
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            4 fromages
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            Sauce tomate à l'origan ou crème fraîche légère, mozzarella, fromage de chèvre, emmental et Fourme d'Ambert AOP
+            </Typography>
+        </CardContent>
+        </CardActionArea>
+        <CardActions>
+        <Button size="small" color="primary">
+            Share
+        </Button>
+        <Button size="small" color="primary">
+            Learn More
+        </Button>
+        </CardActions>
+    </Card>
+        
 )
 
 export default Carte;
+
+*/
+export default function Carte() {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+        <CardActionArea>
+        <CardMedia
+            className={classes.media}
+            image="/assets/img/4fromages.jpg"
+            title="4 fromages"
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            4 fromages
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            Sauce tomate à l'origan ou crème fraîche légère, mozzarella, fromage de chèvre, emmental et Fourme d'Ambert AOP
+            </Typography>
+        </CardContent>
+        </CardActionArea>
+        </Card>
+    )
+}
