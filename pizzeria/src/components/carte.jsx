1d3076eba8257200457828d047bfdc3d34294db9
enzo masson <enzo_2510@hotmail.fr>
2020-10-14 18:01:20 +0200

    Update carte.jsx

--- pizzeria/src/components/carte.jsx
+++ pizzeria/src/components/carte.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,6 +7,9 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 //import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import { Grid } from "@material-ui/core";
+
 
 
 const useStyles = makeStyles({
@@ -16,6 +19,9 @@ const useStyles = makeStyles({
     media: {
       height: 140,
     },
+    root: {
+        flexGrow: 1,
+      },
   });
 
 
@@ -58,13 +64,14 @@ export default Carte;
 
 
 
-
+/*
 
 export default function Carte() {
     const classes = useStyles();
 
+    
     //cela ne fonctionne pas err CORS / err 304 server
-    fetch('http://localhost:3001/pizzas')
+    fetch('http://localhost:3001/pizzas') //pas ouf d'apres le prof, axios meilleur
     .then((response) => {
         return response.json();
     })
@@ -94,8 +101,70 @@ export default function Carte() {
 
             <p>lol</p>
         </div>
-        
-
-        
     )
+
+  
 }
+
+  */
+
+
+  //Cours du prof :) 
+
+  async function makeGetRequest() {
+      
+        let res = await axios.get('http://localhost:3001/pizzas');
+
+        let data = res.data;
+
+        return data;
+  }
+
+  function Carte() {
+
+    const classes = useStyles();
+    const [pizzas, setPizzas] = useState([]);
+    
+    useEffect(() => {
+        fetchPizzas();
+    }, []);
+
+    const fetchPizzas = () =>{
+        makeGetRequest()
+        .then(( data ) => setPizzas(data))
+        .catch((err) => console.log(err))
+    }
+
+    console.log(pizzas);
+
+      return (
+            <div className="Carte">       
+                <Grid container spacing={3}>     
+                    {pizzas.map((value, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index}>                          
+                            <Card className={classes.root}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        className={classes.media}
+                                        image="/assets/img/4fromages.jpg"
+                                        title={value.nom}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            {value.nom}
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            {value.ingredients + " "}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>                   
+                        </Grid>
+                    ))}
+                </Grid>
+
+            </div>
+      );
+  }
+
+  export default Carte;
